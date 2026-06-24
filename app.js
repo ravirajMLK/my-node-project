07b1156f222439543d01a1cfe16aa0c2fc37817e
ravirajMLK <71058048+ravirajMLK@users.noreply.github.com>
2026-06-24 11:16:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const http=require('http');
+
+const server=http.createServer((req,res)=>{
+	res.statusCode=200;
+	res.setHeader('Content-Type','text/plain');
+	res.end("Hello This is my first node.js server app");
+});
+
+// Start the server
+server.listen(3000, () => {
+    console.log(`Server is running at http://localhost:3000`);
+});
